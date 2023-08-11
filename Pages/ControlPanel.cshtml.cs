2091f8f98653cc4b8c37a831f88b223070fe9865
Alvin strandberg <Str4ndb3rg@live.se>
2023-08-11 23:57:32 +0200

    Added Control Panel Page

---
+++ Pages/ControlPanel.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RealEstateApp.Pages
+{
+    public class ControlPanelModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
